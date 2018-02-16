f03e0159baae6c19dc03759ed915ce1572c09242
Sergiy Bondar <bob.zvir@gmail.com>
2018-02-16 23:29:22 +0200

    Show message after send email.

--- ArtliSite/Controllers/HomeController.cs
+++ ArtliSite/Controllers/HomeController.cs
@@ -34,38 +34,38 @@ namespace ArtliSite.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> Contact(ContactModel model)
+        public async Task<ActionResult> SendEmail(ContactModel model)
         {
-            if (ModelState.IsValid)
+            var result = new SendEmailResultModel();
+            try
             {
-                try
-                {
-                    var email = new MailMessage();
-                    email.From = new MailAddress(model.Email);
-                    email.To.Add("artlisoft.info@gmail.com");
-                    email.Subject = model.Subject;
-                    email.Body = model.ToString();
+                var email = new MailMessage();
+                email.From = new MailAddress(model.Email);
+                email.To.Add("artlisoft.info@gmail.com");
+                email.Subject = model.Subject;
+                email.Body = model.ToString();
 
-                    var smtp = new SmtpClient();
-                    smtp.Host = "smtp.gmail.com";
-                    smtp.Port = 587;
-                    smtp.UseDefaultCredentials = false;
-                    smtp.Credentials = new System.Net.NetworkCredential("artlisoft.smm@gmail.com", "qwe123rty!QAZoiu");
-                    smtp.EnableSsl = true;
-                    
-                    await smtp.SendMailAsync(email);
-                    ModelState.Clear();
+                var smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("artlisoft.smm@gmail.com", "qwe123rty!QAZoiu");
+                smtp.EnableSsl = true;
 
-                    ViewBag.Message = "Thank you for Contacting us ";
-                    return RedirectToAction("Index", "Home");
-                }
-                catch (Exception ex)
-                {
-                    ViewBag.Message = $" Sorry we are facing Problem here {ex.Message}";
-                }
+                await smtp.SendMailAsync(email);
+                ModelState.Clear();
+
+                result.IsError = false;
+                result.Message = "Thank you for contacting us. Our manager will contact you in the near time.";
+            }
+            catch (Exception ex)
+            {
+                result.IsError = true;
+                result.Message = " Sorry we are facing some problem. Please, try later again.";
+                //TODO: Add log
             }
 
-            return View(model);
+            return PartialView(result);
         }
 
     }
---
+++ ArtliSite/Models/SendEmailResultModel.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace ArtliSite.Models
+{
+    public class SendEmailResultModel
+    {
+        public string Message { set; get; }
+        public bool IsError { set; get; }
+    }
+}
