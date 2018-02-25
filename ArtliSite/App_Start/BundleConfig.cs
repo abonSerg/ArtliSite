dd1ef82c06c4ffd28582b968bae5da0bb8715766
artem.bondarenko <artem.bondarenko@trinetix.com>
2018-02-25 21:06:09 +0200

    Bundle rendering fix

--- ArtliSite/App_Start/BundleConfig.cs
+++ ArtliSite/App_Start/BundleConfig.cs
@@ -42,7 +42,19 @@ namespace ArtliSite
                 "~/Content/shortcode/header.css",
                 "~/Content/shortcode/preloader.css",
                 "~/Content/shortcode/shortcodes.css",
-                "~/Content/shortcode/slider.css"));
+                "~/Content/shortcode/slider.css").Include(
+                "~/Content/themes/base/jquery.ui.core.css",
+                "~/Content/themes/base/jquery.ui.resizable.css",
+                "~/Content/themes/base/jquery.ui.selectable.css",
+                "~/Content/themes/base/jquery.ui.accordion.css",
+                "~/Content/themes/base/jquery.ui.autocomplete.css",
+                "~/Content/themes/base/jquery.ui.button.css",
+                "~/Content/themes/base/jquery.ui.dialog.css",
+                "~/Content/themes/base/jquery.ui.slider.css",
+                "~/Content/themes/base/jquery.ui.tabs.css",
+                "~/Content/themes/base/jquery.ui.datepicker.css",
+                "~/Content/themes/base/jquery.ui.progressbar.css",
+                "~/Content/themes/base/jquery.ui.theme.css")); ;
             //"~/Content/site.css"));
         }
     }
